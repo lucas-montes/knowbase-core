4c0093a39feacd171fc40985c38601c442d3d8ad
Lucas <lluc23@hotmail.com>
2024-01-28 12:38:35 +0100

    test gzip

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,4 @@
+use crate::engine::prueba;
 use crate::engine::process;
 use std::path::PathBuf;
 
@@ -19,6 +20,14 @@ impl Cli {
             Commands::Add { path } => {
                 todo!("f");
             }
+            Commands::Test {
+                label,
+                item,
+                training,
+            } => {
+                    println!("starting process");
+                prueba(label, item, training).await;
+            }
             Commands::Similarity {
                 label,
                 item,
@@ -38,6 +47,15 @@ enum Commands {
         /// Stuff to add
         #[arg(required = true)]
         path: Vec<PathBuf>,
+    },
+    #[command(arg_required_else_help = true)]
+    Test {
+        #[arg(short, long)]
+        label: String,
+        #[arg(short, long)]
+        item: String,
+        #[arg(short, long)]
+        training: PathBuf,
     },
     #[command(arg_required_else_help = true)]
     Similarity {
--- src/engine.rs
+++ src/engine.rs
@@ -11,31 +11,50 @@ use std::{
     path::PathBuf,
 };
 
-struct GzipClassifierText {
-    label: u32,
-    compressed: Vec<u8>,
-    encoded: Vec<u8>,
-    size: f32,
+use wide::f64x4;
+
+#[derive(Debug)]
+struct GzipClassifierResult {
+    label: usize,
+    result: f64,
 }
 
-impl GzipClassifierText {
-    pub fn from_record(record: &StringRecord) ->  GzipClassifierText {
-        let (text, label) = Self::get_text_label(record);
-        let compressed = compress(&text);
-        let size = compressed.len() as f32;
-        GzipClassifierText {
-            label: label,
-            compressed: compressed,
-            encoded: text,
-            size: size,
-        }
+impl GzipClassifierResult {
+    fn new(first: &[u8], record: &StringRecord) -> Self {
+        let (text, label) = get_text_label(record);
+        let result = ncd(first, &text);
+        Self { label, result }
     }
+    fn sim(first: &[u8], record: &[StringRecord]) -> Vec<Self> {
+        let (text, label) = get_text_label(&record[0]);
+        let result = ncd(first, &text);
+        vec![Self { label, result }]
+    }
+}
 
-    fn get_text_label(record: &StringRecord) -> (Vec<u8>, u32) {
-        let text = get_text(record).as_bytes().to_vec();
-        let label: u32 = record.get(0).expect("no class index").parse().unwrap();
-        (text, label)
+impl Ord for GzipClassifierResult {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.result.partial_cmp(&other.result).unwrap()
     }
+}
+
+impl PartialEq for GzipClassifierResult {
+    fn eq(&self, other: &Self) -> bool {
+        (self.result == other.result) & (self.label == other.label)
+    }
+}
+
+impl Eq for GzipClassifierResult {}
+impl PartialOrd for GzipClassifierResult {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.result.partial_cmp(&other.result)
+    }
+}
+
+fn get_text_label(record: &StringRecord) -> (Vec<u8>, usize) {
+    let text = get_text(record).as_bytes().to_vec();
+    let label: usize = record.get(0).expect("no class index").parse().unwrap();
+    (text, label)
 }
 
 pub fn compress(text: &[u8]) -> Vec<u8> {
@@ -43,9 +62,74 @@ pub fn compress(text: &[u8]) -> Vec<u8> {
     let _ = e.write_all(text);
     e.finish().unwrap()
 }
-pub fn less_is_more(records: &Vec<StringRecord>) -> Vec<Item> {
-    println!("less is more start");
-    records.par_iter().map(|r| calculate(r, records)).collect()
+
+fn calculate(line: &StringRecord, lines: &Vec<StringRecord>) -> Vec<GzipClassifierResult> {
+    let (first, label) = get_text_label(line);
+    lines
+        .par_chunks(4)
+        .flat_map(|l| GzipClassifierResult::sim(&first, &l))
+        .collect::<Vec<GzipClassifierResult>>()
+}
+
+fn get_text(record: &StringRecord) -> String {
+    let mut text = record.get(1).expect("no title").to_owned();
+    text.push_str(" ");
+    text.push_str(record.get(2).expect("no descripton"));
+    return text;
+}
+
+fn ncd(first: &[u8], line: &[u8]) -> f64 {
+    let first_comp = compress(first).len();
+    let second_compress = compress(&line).len();
+    let both_str = [first, "".as_bytes(), line].concat();
+    let both = compress(&both_str).len() as f64;
+    let m = min(first_comp, second_compress) as f64;
+    let ma = max(first_comp, second_compress) as f64;
+    (both - m) / ma
+}
+
+pub async fn prueba(label: String, item: String, training: PathBuf) {
+    let records = get_records(training);
+    let (a, b) = item.split_at(item.len() / 2);
+    let test = StringRecord::from(vec![label, a.to_string(), b.to_string()]);
+    println!("first calculation");
+    let mut sorted_distances = calculate(&test, &records);
+//    sorted_distances.dedup();
+    sorted_distances.par_sort();
+    let (result, _) = sorted_distances.split_at(10); 
+    println!("{:?}", result);
+    let mut f = [0,0,0,0];
+    result.iter().for_each(
+        |r| match r.label {
+            1 => f[0] += 1,
+            2 => f[1] += 1,
+            3 => f[2] += 1,
+            _ => f[3] += 1,
+        }
+    );
+    println!("{:?}", f);
+}
+
+pub async fn process(label: String, item: String, training: PathBuf) {
+    let records = get_records(training);
+    let (a, b) = item.split_at(item.len() / 2);
+    let test = StringRecord::from(vec![label, a.to_string(), b.to_string()]);
+    println!("first calculation");
+    let mut sorted_distances = calculate(&test, &records);
+//    sorted_distances.dedup();
+    sorted_distances.par_sort();
+    let (result, _) = sorted_distances.split_at(10); 
+    println!("{:?}", result);
+    let mut f = [0,0,0,0];
+    result.iter().for_each(
+        |r| match r.label {
+            1 => f[0] += 1,
+            2 => f[1] += 1,
+            3 => f[2] += 1,
+            _ => f[3] += 1,
+        }
+    );
+    println!("{:?}", f);
 }
 
 fn get_records(filepath: PathBuf) -> Vec<StringRecord> {
@@ -55,41 +139,6 @@ fn get_records(filepath: PathBuf) -> Vec<StringRecord> {
         .map(|r| r.unwrap())
         .collect()
 }
-
-fn calculate(line: &StringRecord, lines: &Vec<StringRecord>) -> Item {
-    let (first, label) = GzipClassifierText::get_text_label(line);
-    let values: Vec<f32> = lines
-        .par_iter()
-        .map(|l| ncd(&first, get_text(&l).as_bytes()))
-        .collect();
-    Item::new(label, argsort(&values), None)
-}
-
-fn get_text(record: &StringRecord) -> String {
-    let mut text = record.get(1).expect("no title").to_owned();
-    text.push_str(" ");
-    text.push_str(record.get(2).expect("no descripton"));
-    text
-}
-
-fn ncd(first: &[u8], line: &[u8]) -> f32 {
-    let first_comp = compress(first).len();
-    let second_compress = compress(&line).len();
-    let both_str = [first, "".as_bytes(), line].concat();
-    let both = compress(&both_str).len();
-    ((both - min(first_comp, second_compress)) / max(first_comp, second_compress)) as f32
-}
-
-pub async fn process(label: String, item: String, training: PathBuf) {
-    let records = get_records(training);
-    let (a, b) = item.split_at(item.len() / 2);
-    let test = StringRecord::from(vec![label, a.to_string(), b.to_string()]);
-    println!("first calculation");
-    let result = KNN::new(calculate(&test, &records), less_is_more(&records), 10)
-        .result(SimilarityAlgos::Cosine);
-    println!("{:?}", result);
-}
-
 async fn tf(paths: Vec<PathBuf>) {
     for filepath in paths {
         let word_count = get_word_count(&filepath);
